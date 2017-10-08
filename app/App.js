bb7e26227643029870b65952c104680c38e17872
Patrick Liesveld <pliesveld@users.noreply.github.com>
2017-10-07 21:45:43 -0400

    moved items list to external file

--- app/App.js
+++ app/App.js
@@ -4,13 +4,29 @@ import styles from './App.css';
 import ItemList from './ItemList';
 
 class App extends Component{
+  constructor() {
+    super();
+    this.state = {
+      items: []
+    };
+  }
+
   render() {
     return (
        <div id='root'>
-        <ItemList items={this.props.items}/>
+        <ItemList items={this.state.items}/>
        </div>
     );
   }
+
+  componentDidMount() {
+    fetch('./items.json')
+    .then((response) => response.json())
+    .then((responseData) => {
+      this.setState({items: responseData});
+    });
+  }
+
 }
 
 App.propTypes = {
